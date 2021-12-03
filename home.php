08c6a2116bfcf688930ca68677ef8ce5fc01e450
Tajouri Med Amine <60980991+Tajouri213@users.noreply.github.com>
2021-12-03 05:41:37 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Home</title>
+</head>
+<style>
+.table td, .table th{
+	text-align:center;
+}
+</style>
+<body>
+
+<div class="container">
+
+    <h5 style='text-align:center;'>Les Sorties</h5>
+    <hr/>
+    <table class="table table-bordered">
+            <thead class="alert-info">
+                <tr>
+                    <th>Date </th>
+                    <th>Localisation</th>
+                    <th>Nom de bateau</th>
+                    <th>Nom de sauvteur</th>
+                    <th>Nombre des sauvé</th>
+                    <th>Nombre décédés</th>
+                    <th>Témoiniage</th>				
+                </tr>
+            </thead>
+            
+                
+        </table>
+
+</div>
+
+
+
+
+
+</body>
+</html>
